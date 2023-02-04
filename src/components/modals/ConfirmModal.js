d718717d1956f1efd1c02d92d620e19b1c7fd6f4
ManjiCoder <manjicoder@gmail.com>
2023-02-05 01:57:44 +0530

    Confirm - Modal ✅ Now ConfirmModal.js is using Headless Ui

--- src/components/modals/ConfirmModal.js
+++ src/components/modals/ConfirmModal.js
@@ -1,18 +1,12 @@
-import React, { useEffect } from "react";
-import reactDom from "react-dom";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
-const ConfirmModal = ({ closeModal, deleteCardId, APIKEY, alertTodo }) => {
-  // TO TOGGLE SCROLL
-  useEffect(() => {
-    document.body.style.overflowY = "hidden";
-    document.getElementById("root").style.filter = "brightness(0.5)";
-
-    return () => {
-      document.body.style.overflowY = "scroll";
-      document.getElementById("root").style.filter = "brightness(1)";
-    };
-  }, []);
-
+export default function ConfirmModal({
+  closeModal,
+  deleteCardId,
+  APIKEY,
+  alertTodo,
+}) {
   // DELETE - REQUEST
   const deleteRow = async (id) => {
     // console.log(id, APIKEY);
@@ -22,16 +16,13 @@ const ConfirmModal = ({ closeModal, deleteCardId, APIKEY, alertTodo }) => {
         "Accept": "application/json",
         "Content-Type": "application/json",
       },
-    })
+    });
     console.log(res.ok);
     alertTodo("Deleted", res.ok);
     if (res.ok) {
       let response = await res.json();
       console.log(response);
-      window.location = "/";
-      // setTimeout(() => {
-      //   window.location = '/'
-      // }, 1000);
+      document.getElementById(id).remove()
     } else {
       throw Error(res.message);
     }
@@ -40,87 +31,62 @@ const ConfirmModal = ({ closeModal, deleteCardId, APIKEY, alertTodo }) => {
     closeModal();
     deleteRow(deleteCardId);
   };
-  // console.log(deleteCardId);
-  return reactDom.createPortal(
+
+  return (
     <>
-      {/* Modal Wrapper */}
-      <div
-        className="fixed top-0 left-0 right-0 bottom-0 backdrop-blur-sm"
-        onClick={closeModal}
-      ></div>
-      <div
-        id="popup-modal"
-        tabIndex="-1"
-        className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-50 p-4"
-      >
-        <div className="relative w-[20rem] h-full md:w-96 md:h-auto">
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button
-              type="button"
-              onClick={closeModal}
-              className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-              data-modal-hide="popup-modal"
-            >
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+      <Transition appear show={true} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span className="sr-only">Close modal</span>
-            </button>
-            <div className="p-6 text-center">
-              <svg
-                aria-hidden="true"
-                className="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
-              </svg>
-              <h3 className="m3 text-lg font-normal text-gray-500 dark:text-gray-400">
-                Are you sure?
-              </h3>
-              <h4 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                You want to delete Card No.{deleteCardId}?
-              </h4>
-              <button
-                data-modal-hide="popup-modal"
-                type="button"
-                onClick={handleOnDelete}
-                className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
-              >
-                Yes, I'm sure
-              </button>
-              <button
-                data-modal-hide="popup-modal"
-                type="button"
-                onClick={closeModal}
-                className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-              >
-                No, cancel
-              </button>
+                <Dialog.Panel className="w-11/12 max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all flex flex-col items-center dark:bg-slate-700 dark:text-white">
+                  <Dialog.Title as="h3">
+                    <i className="mb-3 fa-solid fa-circle-exclamation text-5xl text-red-600"></i>
+                  </Dialog.Title>
+                  <h2 className="text-2xl font-medium">Are you sure?</h2>
+                  <p>You want to delete Card No.{deleteCardId}</p>
+                  <div className="w-full mt-4 flex justify-evenly">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-700 focus-visible:ring-offset-2"
+                      onClick={handleOnDelete}
+                    >
+                      Yes,I'm sure!
+                    </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      No, cancel!
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
             </div>
           </div>
-        </div>
-      </div>
-    </>,
-    document.getElementById("ParentModalDiv")
+        </Dialog>
+      </Transition>
+    </>
   );
-};
-
-export default ConfirmModal;
+}
