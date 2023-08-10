14d1717ec7879520cb75c85dce40ccffd87092d4
ManjiCoder <manjicoder@gmail.com>
2023-08-11 01:38:52 +0530

    Favicon added - ✔

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+// SEO IMP Component
 import Head from "next/head";
 import React from "react";
 
@@ -14,8 +15,32 @@ export default function Header({ title, description }) {
       ) : (
         <meta name="description" content={description} />
       )}
+      <meta
+        name="keywords"
+        content="Next.Js, React.Js, Coding-Journal, Redux"
+      />
+      <meta name="authur" content="Manji coder" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
   );
 }
+
