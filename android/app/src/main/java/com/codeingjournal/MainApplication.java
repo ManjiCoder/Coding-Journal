84645276142af831fcd3c3008b1cdc093f02f0d5
Manji Coder <manjicoder@gmail.com>
2024-11-13 12:37:38 +0530

    Revert "rename"
    This reverts commit b25c1163f8febbc00295f8a5a374befe4e23f5a4.

--- android/app/src/main/java/com/codeingjournal/MainApplication.java
+++ android/app/src/main/java/com/codeingjournal/MainApplication.java
@@ -1,4 +1,4 @@
-package com.CodingJournal;
+package com.codeingjournal;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/CodeingJournalTests/CodeingJournalTests.m
+++ ios/CodeingJournalTests/CodeingJournalTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface CodingJournalTests : XCTestCase
+@interface CodeingJournalTests : XCTestCase
 
 @end
 
-@implementation CodingJournalTests
+@implementation CodeingJournalTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
