721371f04d2566e2b95f99f23f617aa154262eca
Manji Coder <manjicoder@gmail.com>
2024-11-14 17:11:31 +0530

    React Query

--- index.js
+++ index.js
@@ -2,8 +2,19 @@
  * @format
  */
 
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+const queryClient = new QueryClient();
+export default function Main() {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </>
+  );
+}
+AppRegistry.registerComponent(appName, () => Main);
